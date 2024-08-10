f0b5752a01a400d8b2facc49312fa2ecbd207812
FilippoCarlone11 <carlonefilippomm@gmail.com>
2024-08-10 10:57:37 +0200

    esercizio completato

--- LAB03/es02.c
+++ LAB03/es02.c
@@ -2,35 +2,48 @@
 #include <stdlib.h>
 
 void LoadData(FILE *fp, int nFriends, int **FriendsChosen, char ****songs);
-int princ_molt(int cnt, int *sol, int* FriendsChosen, int pos, int numberOfFriends, char ***matSongs);
+int princ_molt(int cnt, int *sol, int* nChoicesForFriends, int pos, int numberOfFriends, char ***matSongs);
+
+
 int main(void){
+    int numberOfFriends, checkCNT;
+
+    //FILE ACTIVITIES
     FILE *fp = fopen("brani.txt", "r");
-    int numberOfFriends, cntmolt;
-    int *nScelte;
-    char ***matSongs;
     fscanf(fp, "%d", &numberOfFriends);
-    LoadData(fp, numberOfFriends, &nScelte, &matSongs);
 
-   /* for(int i = 0; i< numberOfFriends; i++) {
-        for (int j = 0; j < nScelte[i]; j++)
-            printf("%s ", matSongs[i][j]);
-        printf("\n");
-    }*/
-   int *sol = (int *)malloc(5*sizeof(int));
+    //LOAD DATA
+    int *choicesPerFriend;
+    char ***matSongs;
+    LoadData(fp, numberOfFriends, &choicesPerFriend, &matSongs);
 
-   cntmolt = princ_molt(0, sol, nScelte, 0, numberOfFriends, matSongs);
-   printf("%d ", cntmolt);
+
+    int *sol = (int *)malloc(5*sizeof(int));
+    checkCNT = princ_molt(0, sol, choicesPerFriend, 0, numberOfFriends, matSongs); //The value should be 900 (5*3*4*5*3)
+    printf("%d ", checkCNT);
+
+
+    //FREE
+    free(sol);
+    for(int i = 0; i<numberOfFriends; i++){
+        for(int j = 0; j < choicesPerFriend[i]; j++)
+            free(matSongs[i][j]);
+        free(matSongs[i]);
+    }
+    free(choicesPerFriend);
+    free(matSongs);
 }
 
-void LoadData(FILE *fp, int nFriends, int **FriendsChosen, char ****songs){
-    *FriendsChosen = (int*)malloc(nFriends * sizeof(int));
+void LoadData(FILE *fp, int nFriends, int **nChoicesForFriends, char ****songs){
+// I want to simultaneously save the number of songs chosen by each friend in nChoicesForFriends and the list of songs chosen in songs. I passed pointers to the variables because I need to return 2 data structures.
+    *nChoicesForFriends = (int*)malloc(nFriends * sizeof(int));
     *songs = (char***)malloc(nFriends*sizeof(char**));
 
     for(int i = 0; i < nFriends; i++){
-        fscanf(fp, "%d", &(*FriendsChosen)[i]);
-        (*songs)[i] = (char**)malloc((*FriendsChosen)[i] * sizeof(char*));
+        fscanf(fp, "%d", &(*nChoicesForFriends)[i]);   // Note that "*nChoisesForFriends" is the variable I am using
+        (*songs)[i] = (char**)malloc((*nChoicesForFriends)[i] * sizeof(char*));
 
-        for(int j = 0; j < (*FriendsChosen)[i]; j++){
+        for(int j = 0; j < (*nChoicesForFriends)[i]; j++){
             (*songs)[i][j] = (char*) malloc(255*sizeof(char));
             fscanf(fp, "%s", (*songs)[i][j]);
         }
@@ -46,7 +59,7 @@ int princ_molt(int cnt, int *sol, int* FriendsChosen, int pos, int numberOfFrien
         printf("\n");
         return cnt + 1;
     }
-    for(j = 0; j < FriendsChosen[pos]; j++){
+    for(j = 0; j < FriendsChosen[pos]; j++){  //note that I'm iterating over each friend and there is no data structure other than the variable 'j'
         sol[pos] = j;
         cnt = princ_molt(cnt, sol, FriendsChosen, pos+1, numberOfFriends, matSongs);
     }
